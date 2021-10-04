c02fab66c4aa653ed421c9c09e809fe95255c090
Bilal Fawaz <bilal.fawaz@student.ehb.be>
2021-10-04 22:52:26 +0200

    Exercice1

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ window.onload = function () {
     console.log('loaded');
 
 
-    document.getElementById('form')
-        .addEventListener('submit', event => {
-            event.preventDefault();
+    document.getElementById('form').addEventListener('submit', event => {
+        event.preventDefault();
+        let name = document.getElementById('nameInput').value;
+        let email = document.getElementById('emailInput').value;
+        let order = document.getElementById('orderInput').value;
+        console.log(name, email, order);
 
-            let name = document.getElementById('nameInput').value;
-            let email = document.getElementById('emailInput').value;
-            let order = document.getElementById('');
-        })
-}
+        document.getElementById('container').innerHTML = `<p> The order from the customer <b> ${name} </b> with the following <b> ${order} </b>, notify by email: <b>${email}</b></p>`
+    });
+};
