89a9a327df6b23c608c5e3866567a7882749fc25
Bilal Fawaz <bilal.fawaz@student.ehb.be>
2021-10-06 14:08:54 +0200

    verbetering

--- oef2/team.js
+++ oef2/team.js
@@ -1,16 +1,4 @@
 class Team {
-    constructor(teamname) {
-        this.teamname = teamname;
-        this.trainername = 'Ash';
-        this.pokemonTeam = [];
-    }
-
-    describe() {
-        return `${teamname} with trainer ${this.trainername}`;
-    }
-}
-
-class Team {
     constructor(teamname, trainer, roster)
     this.teamname = 'Example teamname';
     this.trainer = 'Example trainername';
