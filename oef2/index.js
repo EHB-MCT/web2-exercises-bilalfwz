9f46a612027416c580b6c52d8b444e345491832d
Bilal Fawaz <bilal.fawaz@student.ehb.be>
2021-10-06 14:01:01 +0200

    oef2pokemon

---
+++ oef2/index.js
@@ -0,0 +1,25 @@
+let list = [];
+let pokemons = [];
+fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+    .then(resp => resp.json())
+    .then(data => {
+        list = data.results;
+        // LOOP OVER EVERY POKEMON
+        list.forEach(poke => {
+            fetch(poke.url)
+                .then(resp => resp.json())
+                .then(pokeObject => {
+                    pokemons.push(pokeObject);
+
+                });
+        });
+
+    });
+
+window.onload = function () {
+    setTimeout(buildList, 3000);
+
+    function buildList() {
+        console.log(pokemons);
+    }
+}
